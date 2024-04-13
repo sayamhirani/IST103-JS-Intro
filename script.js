597d48f2bee009acff593fcef87f6bb95c34e8b4
Sayam <sayamhirani01@gmail.com>
2024-04-12 21:29:49 -0700

    Activity 1, 2 & 3 added

---
+++ script.js
@@ -0,0 +1,29 @@
+// alert("Hello World");
+
+// Activity 2
+
+let personName = "virat";
+let personAge = "38";
+let personiscricketer = true;
+let weight = 61;
+let hobbies;
+let address = null;
+
+console.log("A random person name is: " + personName);
+console.log("Person age is " + personAge);
+console.log("Is the person cricketer" + personiscricketer);
+console.log("Person weight is" + weight);
+console.log("person hobbies" + hobbies);
+console.log("Persone address is" + address);
+
+// Activity 3
+let todolist = ["Find a job", "go to BC office", "change mail"];
+
+let myTv = {
+  brand: "Sony",
+  color: "black",
+  isUsed: true,
+};
+
+console.log("my first to do is: " + todolist[0]);
+console.log("my backpack color is: " + myTv.color);
